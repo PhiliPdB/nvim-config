167935758cf505f60745c41a2138017c82fa9f31
PhiliPdB <phlpdbrn@gmail.com>
2025-09-27 13:23:00 +0200

    Add lazy package manager and start with plugin setup

--- init.lua
+++ init.lua
@@ -3,10 +3,11 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 require('options')
 require('keymaps')
+require('autocmds')
 require('plugin-manager')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
---
+++ lua/plugins/colortheme.lua
@@ -0,0 +1,23 @@
+return {
+  'marko-cerovac/material.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    require('material').setup({
+      contrast = {
+        terminal = true,
+        sidebars = true,
+        floating_windows = true,
+        cursor_line = false,
+        lsp_virtual_test = true,
+      },
+
+      disable = {
+        background = true,
+      },
+    })
+
+    vim.g.material_style = 'darker'
+    vim.cmd.colorscheme('material')
+  end,
+}
+
