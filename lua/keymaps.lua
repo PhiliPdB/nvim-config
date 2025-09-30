a2a42fa05a5e32948598671025feebb7c19d4897
PhiliPdB <phlpdbrn@gmail.com>
2025-09-30 19:28:43 +0200

    Change options, add remaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,6 +10,19 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+-- <C-d> and <C-u> keep cursor centered
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- Keep search terms centered
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Quick copy to clipboard : asbjornHaland
+vim.keymap.set('n', '<leader>y', '\"+y')
+vim.keymap.set('v', '<leader>y', '\"+y')
+vim.keymap.set('n', '<leader>Y', '\"+Y')
+
+
 -- NOTE: LSP keymaps are set in lspconfig.lua when LSP attaches
 
 
--- lua/options.lua
+++ lua/options.lua
@@ -9,25 +9,19 @@ vim.o.shiftwidth = 4
 vim.o.smarttab = true
 vim.o.expandtab = true
 
-
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
 -- Don't show the mode, it's in the status line
 -- vim.o.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
--- vim.schedule(function()
---   vim.o.clipboard = 'unnamedplus'
--- end)
-
 -- Enable break indent
 vim.o.breakindent = true
 
 -- Save undo history
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
@@ -58,9 +52,6 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
-
 -- Show which line your cursor is on
 vim.o.cursorline = true
 
@@ -73,5 +64,8 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 
 -- Set highlight on search (clear on pressing <Esc> in normal mode)
-vim.o.hlsearch = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
 
