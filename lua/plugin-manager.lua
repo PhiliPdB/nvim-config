456881f60cb1388317ed8ee39272163281d93876
PhiliPdB <phlpdbrn@gmail.com>
2025-10-02 20:43:14 +0200

    Render markdown

--- lua/plugin-manager.lua
+++ lua/plugin-manager.lua
@@ -1,16 +1,15 @@
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 -- https://github.com/folke/lazy.nvim
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -18,8 +17,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  { import = 'plugins' }
+require('lazy').setup({
+  { import = 'plugins' },
+  { import = 'plugins.markdown' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -39,7 +39,5 @@ require("lazy").setup({
       task = '📌',
       lazy = '💤 ',
     },
-  }
+  },
 })
-
-
