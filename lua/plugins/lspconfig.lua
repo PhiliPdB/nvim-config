0dce15c6efef78a97250cd2a9b4ec8b45e1d0b1c
PhiliPdB <phlpdbrn@gmail.com>
2025-10-04 19:59:03 +0200

    Setup lualine

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -70,7 +70,7 @@ return {
         ---@param bufnr? integer some lsp support methods only in specific files
         ---@return boolean
         local function client_supports_method(client, method, bufnr)
-          if vim.fn.has 'nvim-0.11' == 1 then
+          if vim.fn.has('nvim-0.11') == 1 then
             return client:supports_method(method, bufnr)
           else
             return client.supports_method(method, { bufnr = bufnr })
@@ -101,7 +101,7 @@ return {
             group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
             callback = function(event2)
               vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+              vim.api.nvim_clear_autocmds({ group = 'lsp-highlight', buffer = event2.buf })
             end,
           })
         end
@@ -112,7 +112,7 @@ return {
         -- This may be unwanted, since they displace some of your code
         if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
           map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
           end, '[T]oggle Inlay [H]ints')
         end
       end,
@@ -120,16 +120,16 @@ return {
 
     -- Diagnostic Config
     -- See :help vim.diagnostic.Opts
-    vim.diagnostic.config {
+    vim.diagnostic.config({
       severity_sort = true,
       float = { border = 'rounded', source = 'if_many' },
       underline = { severity = vim.diagnostic.severity.ERROR },
       signs = vim.g.have_nerd_font and {
         text = {
-          [vim.diagnostic.severity.ERROR] = '󰅚 ',
-          [vim.diagnostic.severity.WARN] = '󰀪 ',
-          [vim.diagnostic.severity.INFO] = '󰋽 ',
-          [vim.diagnostic.severity.HINT] = '󰌶 ',
+          [vim.diagnostic.severity.ERROR] = ' ',
+          [vim.diagnostic.severity.WARN] = ' ',
+          [vim.diagnostic.severity.INFO] = '󰋼 ',
+          [vim.diagnostic.severity.HINT] = '󰌵 ',
         },
       } or {},
       virtual_text = {
@@ -145,7 +145,7 @@ return {
           return diagnostic_message[diagnostic.severity]
         end,
       },
-    }
+    })
 
     -- LSP servers and clients are able to communicate to each other what features they support.
     --  By default, Neovim doesn't support everything that is in the LSP specification.
@@ -166,7 +166,7 @@ return {
       -- Create server config with the blink.cmp capabilities broadcasted
       -- Note that remaining server config is in the `lua/` directory
       local server = {
-        capabilities = vim.tbl_deep_extend('force', {}, capabilities, {})
+        capabilities = vim.tbl_deep_extend('force', {}, capabilities, {}),
       }
       local cfg = vim.lsp.config[server_name]
 
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,40 @@
+local diagnostics = {
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  sections = { 'error', 'warn', 'info' },
+  symbols = {
+    error = ' ',
+    warn = ' ',
+    info = '󰋼 ',
+    hint = '󰌵 ',
+  },
+  colored = true,
+  update_in_insert = false,
+  always_visible = false,
+  cond = function()
+    return vim.bo.filetype ~= 'markdown'
+  end,
+}
+
+local diff = {
+  'diff',
+  colored = true,
+  symbols = { added = ' ', modified = ' ', removed = ' ' },
+}
+
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', diff },
+        lualine_c = { 'filename', diagnostics },
+        lualine_x = { 'fileformat', 'filetype' },
+        lualine_y = { 'lsp_status' },
+        lualine_z = { 'location' },
+      },
+    })
+  end,
+}
