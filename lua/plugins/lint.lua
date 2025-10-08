3ce1b48637c11280479bf3cd851a678b4f6c0831
PhiliPdB <phlpdbrn@gmail.com>
2025-10-08 20:48:14 +0200

    Disable linting on markdown files for now

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,6 +5,7 @@ return {
     local lint = require('lint')
 
     lint.linters_by_ft = lint.linters_by_ft or {}
+    lint.linters_by_ft['markdown'] = {}
     lint.linters_by_ft['rust'] = { 'clippy' }
 
     -- Create autocommand which carries out the actual linting
