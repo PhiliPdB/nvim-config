597d0f10c406a42c80e9fbe37bb4ddefede46900
PhiliPdB <phlpdbrn@gmail.com>
2026-01-24 20:06:32 +0100

    Setup debugger
    closes #37

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -82,7 +82,7 @@ return {
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 
-    vim.keymap.set('n', '<leader>b', function()
+    vim.keymap.set('n', '<leader>n', function()
       builtin.buffers({
         ignore_current_buffer = true,
         sort_lastused = true,
